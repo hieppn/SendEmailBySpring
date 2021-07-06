d3905e6e0dc09781f93c865a68e85d6b47ae61b4
hieppn <hiep.bui21@student.passerellesnumeriques.org>
2021-07-06 15:45:24 +0700

    demo

---
+++ src/main/java/stackjava/com/springsendemail/demo/MainApp.java
@@ -0,0 +1,21 @@
+package stackjava.com.springsendemail.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class MainApp {
+	public static void main(final String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		MailSender mailSender = (MailSender) context.getBean("mailSender2");
+		System.out.println("Sending an email to your email....");
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("gogo.team2021@gmail.com");
+		message.setTo("buihuuhiepdev@gmail.com");
+		message.setSubject("Demo send email in spring");
+		message.setText("Hello, I am Hiep, I am a be developer.");
+		mailSender.send(message);
+		System.out.println("Done!");
+		context.close();
+	}
+}
